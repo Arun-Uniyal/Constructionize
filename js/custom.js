f48ec9e3906f1028130231f99a6130b55f15bf54
Arun Uniyal <er.arun.uniyal@gmail.com>
2020-04-22 21:51:24 +0530

    English Base Structure

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+
+jQuery(document).ready(function () {
+    // $('.input-field input').focus(function () {
+    //     $(this).parent().addClass('is-active');
+    // });
+    // $('.input-field input').focusout(function () {
+    //     if ($(this).val() === "") {
+    //         $(this).parent().removeClass('is-active');
+    //     }
+
+    // });
+});
